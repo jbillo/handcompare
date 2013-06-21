437bd1eac4160d198f907d2d03bf4c5d3bbc040d
Jake Billo <jake@jakebillo.com>
2013-06-21 01:29:16 -0400

    Added more default hands

--- default_hands.py
+++ default_hands.py
@@ -2,8 +2,19 @@
 
 DEFAULT_HANDS = {
     "royal_flush"               : "10C,JC,QC,KC,AC",
+    "royal_flush_2"             : "10D,JD,QD,KD,AD",
     "royal_flush_unsorted"      : "KD,QD,AD,10D,JD",
     "straight_flush"            : "9S,10S,JS,QS,KS",
     "straight_flush_ace_low"    : "2H,3H,4H,5H,AH",
     "straight_flush_numbers"    : "5C,6C,8C,7C,9C",
+    "straight_flush_2"          : "9D,10D,JD,QD,KD",
+    "four_of_a_kind"            : "4C,4D,4H,4S,5C",
+    "four_of_a_kind_5s"         : "5C,5D,5H,5S,6C",
+    "full_house"                : "QC,QD,QS,JH,JS",
+    "flush"                     : "AS,JS,9S,8S,2S",
+    "straight"                  : "5S,6C,7D,8H,9S",
+    "straight_ace_low"          : "AC,2D,3C,4H,5S",
+    "two_pair"                  : "9C,9H,7C,7H,2S",
+    "pair"                      : "JS,JD,5C,6H,9D",
+    "high_card"                 : "KS,JD,9S,5H,4H",
 }
